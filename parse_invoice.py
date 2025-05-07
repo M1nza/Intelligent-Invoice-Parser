b6ebf8af61f68e34505fe8663a1ed5d45aba8342
Minza Sadiq <140895481+M1nza@users.noreply.github.com>
2025-05-07 13:56:48 +0400

    parse_invoice.py

--- parse_invoice.py
+++ parse_invoice.py
@@ -4,19 +4,16 @@ from utils import extract_fields
 
 def parse_invoice(pdf_path, output_json):
     full_text = ""
-
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
             full_text += page.extract_text() + "\n"
 
     data = extract_fields(full_text)
 
-    # Save output
     with open(output_json, "w") as f:
         json.dump(data, f, indent=4)
 
     print(f"Extracted data saved to {output_json}")
 
-# Sample usage
 if __name__ == "__main__":
     parse_invoice("sample_invoice.pdf", "cleaned_invoice.json")
